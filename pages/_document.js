47094dbf373ff97368156d10c92b7e46167d9377
Jesús <98342185+bcondict@users.noreply.github.com>
2023-06-08 00:07:00 +0000

    correction

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class Document extends NextDocument {
         <Head />
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <main />
+          <Main />
           <NextScript />
         </body>
       </Html>
